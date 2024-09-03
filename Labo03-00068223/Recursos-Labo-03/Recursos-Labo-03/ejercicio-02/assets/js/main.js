d3662c51cd0b790b26a53b0411901b65fce69f89
JavierCaceres5 <00068223@uca.edu.sv>
2024-09-03 11:40:34 -0600

    Ejercicio 2 terminado

--- Labo03-00068223/Recursos-Labo-03/Recursos-Labo-03/ejercicio-02/assets/js/main.js
+++ Labo03-00068223/Recursos-Labo-03/Recursos-Labo-03/ejercicio-02/assets/js/main.js
@@ -1,57 +1,55 @@
 
 const plus = (numero1, numero2) => {
     var suma = numero1 + numero2;
-    alert(suma);
+    alert('El resultado de la suma es: ' + suma);
 };
 const subtract = (numero1, numero2) =>{
     var resta = numero1 - numero2;
-    return resta;
+    alert('EL resultado de la resta es: ' + resta);
 } ;
 const multiply = (numero1, numero2) =>{
     var multiplicacion = numero1 * numero2;
-    return multiplicacion;
+    alert('EL resultado de la multiplicacion es: ' + multiplicacion);
 } ;
 const split = (numero1, numero2) => {
     var division = numero1 / numero2;
-    return division;
-    aler
+    alert('El resultado de la division es: ' + division);
 };
 const requestNumber = () => {
     var numero1 = parseInt(prompt("Ingrese el primer numero ", 0));
     var numero2 = parseInt(prompt("Ingrese el segundo numero: ", 0))
-    let resultado = [numero1, numero2];
-    return resultado;
+    return [numero1, numero2];
 };
 
-const selectOperation = () => {
+const selectOperation = (numero1, numero2) => { 
     var opcion = parseInt(prompt("Introduce una opcion: " + 
-                          "1. Suma" +
-                          "2. Resta" +
-                          "3. Multiplicacion" + 
+                          "1. Suma " +
+                          "2. Resta " +
+                          "3. Multiplicacion " + 
                           "4. Division"));
 
     switch (opcion) {
-        case '1':
-            plus(number1, number2);
+        case 1:
+            plus(numero1, numero2);
             break;
-        case '2':
-            subtract(number1, number2);
+        case 2:
+            subtract(numero1, numero2);
             break;
-        case '3':
-            multiply(number1, number2);
+        case 3:
+            multiply(numero1, numero2);
             break;
-        case '4':
-            split(number1, number2);
+        case 4:
+            split(numero1, numero2);
             break;
         default:
-            alert('Ingrese un numero valido');
+            alert('Ingrese una opcion valida');
             break;
     }
 };
 
 const main = () => {
-    requestNumber();
-    selectOperation(); 
+    const [numero1,numero2] = requestNumber();
+    selectOperation(numero1, numero2); 
 }
 
 main();

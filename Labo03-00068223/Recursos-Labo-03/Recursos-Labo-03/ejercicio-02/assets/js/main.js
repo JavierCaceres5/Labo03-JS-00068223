f7d57a628ba0965d2e72a63bd4ea772e2eafb037
JavierCaceres5 <00068223@uca.edu.sv>
2024-09-03 10:31:42 -0600

    Ejercicio 1

---
+++ Labo03-00068223/Recursos-Labo-03/Recursos-Labo-03/ejercicio-02/assets/js/main.js
@@ -0,0 +1,29 @@
+
+const plus = (/*recibe*/) => {
+    //Code
+};
+const subtract = (/*recibe*/) =>{
+    //Code
+} ;
+const multiply = (/*recibe*/) =>{
+    //Code
+} ;
+const split = (/*recibe*/) => {
+    //Code
+};
+const requestNumber = () => {
+    var numero1 = parseInt(prompt("Ingrese el primer numero ", 0));
+    var numero2 = parseInt(prompt("Ingrese el segundo numero: ", 0))
+
+    return numero1, numero2;
+};
+
+const selectOperation = () => {
+    var opcion = parseInt(prompt("Introduce una opcion: "))
+};
+
+const main = () => {
+    requestNumber();
+}
+
+main();

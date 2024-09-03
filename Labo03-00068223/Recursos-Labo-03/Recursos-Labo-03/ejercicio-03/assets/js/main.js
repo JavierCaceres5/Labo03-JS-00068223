aa803acfa5649ef548576641c0810dfeeac27f6e
JavierCaceres5 <00068223@uca.edu.sv>
2024-09-03 11:55:27 -0600

    Ejercicio 3

--- Labo03-00068223/Recursos-Labo-03/Recursos-Labo-03/ejercicio-03/assets/js/main.js
+++ Labo03-00068223/Recursos-Labo-03/Recursos-Labo-03/ejercicio-03/assets/js/main.js
@@ -1,13 +1,21 @@
-const calculateFactorial = (/*recibe*/) => {
-    //Code
+const calculateFactorial = (numero1) => {
+    let factorial = 1;
+    for(let i = 1; i <= numero1; i++){
+        factorial *= i;
+    }
+    return factorial;
+    
 }
 
 const requestNumber = () => {
-    //Code
+    var numero1 = parseInt(prompt("Ingrese un numero: ", 0));
+    return numero1;
 }
 
 const main = () => {
-    //Code
+    const numero1 =requestNumber();
+    const result = calculateFactorial(numero1);
+    alert('El factorial de: ' + numero1 + ' (iterativo) es: ' +  result);
 }
 
 main();

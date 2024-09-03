1ef6ee4a4bfd2a6f61b5fcd67e7891b2ebf302cd
JavierCaceres5 <00068223@uca.edu.sv>
2024-09-03 10:51:03 -0600

    Ejercicio 2 casi terminado

--- Labo03-00068223/Recursos-Labo-03/Recursos-Labo-03/ejercicio-02/assets/js/main.js
+++ Labo03-00068223/Recursos-Labo-03/Recursos-Labo-03/ejercicio-02/assets/js/main.js
@@ -1,29 +1,57 @@
 
-const plus = (/*recibe*/) => {
-    //Code
+const plus = (numero1, numero2) => {
+    var suma = numero1 + numero2;
+    alert(suma);
 };
-const subtract = (/*recibe*/) =>{
-    //Code
+const subtract = (numero1, numero2) =>{
+    var resta = numero1 - numero2;
+    return resta;
 } ;
-const multiply = (/*recibe*/) =>{
-    //Code
+const multiply = (numero1, numero2) =>{
+    var multiplicacion = numero1 * numero2;
+    return multiplicacion;
 } ;
-const split = (/*recibe*/) => {
-    //Code
+const split = (numero1, numero2) => {
+    var division = numero1 / numero2;
+    return division;
+    aler
 };
 const requestNumber = () => {
     var numero1 = parseInt(prompt("Ingrese el primer numero ", 0));
     var numero2 = parseInt(prompt("Ingrese el segundo numero: ", 0))
-
-    return numero1, numero2;
+    let resultado = [numero1, numero2];
+    return resultado;
 };
 
 const selectOperation = () => {
-    var opcion = parseInt(prompt("Introduce una opcion: "))
+    var opcion = parseInt(prompt("Introduce una opcion: " + 
+                          "1. Suma" +
+                          "2. Resta" +
+                          "3. Multiplicacion" + 
+                          "4. Division"));
+
+    switch (opcion) {
+        case '1':
+            plus(number1, number2);
+            break;
+        case '2':
+            subtract(number1, number2);
+            break;
+        case '3':
+            multiply(number1, number2);
+            break;
+        case '4':
+            split(number1, number2);
+            break;
+        default:
+            alert('Ingrese un numero valido');
+            break;
+    }
 };
 
 const main = () => {
     requestNumber();
+    selectOperation(); 
 }
 
 main();
